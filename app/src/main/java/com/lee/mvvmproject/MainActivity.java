b5a814278ced2cc87abbee30fb27d8ac68eee2b6
李宇航 <985530812@qq.com>
2018-12-26 16:01:35 +0800

    [add] LiveData Support

--- app/src/main/java/com/lee/mvvmproject/MainActivity.java
+++ app/src/main/java/com/lee/mvvmproject/MainActivity.java
@@ -1,8 +1,30 @@
 package com.lee.mvvmproject;
 
+import android.os.Bundle;
+
+import com.lee.mvvm.base.constract.IBaseViewModel;
 import com.lee.mvvm.base.view.BaseActivity;
 
 
 public class MainActivity extends BaseActivity {
 
+    @Override
+    protected int bindView() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected IBaseViewModel initVM() {
+        return null;
+    }
+
+    @Override
+    protected void loadData(Bundle dataFromIntent) {
+
+    }
 }
