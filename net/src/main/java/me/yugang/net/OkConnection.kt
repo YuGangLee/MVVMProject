4eadd0a0688ddf43014e905c6d207a42f397313b
Lee <yuhanglee1996@gmail.com>
2021-01-08 11:28:48 +0800

    feat: 修改网络模块部分API格式

--- net/src/main/java/me/yugang/net/OkConnection.kt
+++ net/src/main/java/me/yugang/net/OkConnection.kt
@@ -1,11 +1,32 @@
 package me.yugang.net
 
+import okhttp3.OkHttpClient
 import okhttp3.Request
 
 object OkConnection {
-    var httpClient = DefaultClient.httpClient
+    private var mHttpClient = DefaultClient.httpClient
 
-    fun newRequest(request: Request) = RequestHolder(request)
+    val httpClient get() = mHttpClient
 
-    fun newSycnRequest(request: Request) = SyncRequestHolder(request)
+    @JvmStatic
+    @JvmOverloads
+    fun create(url: String = "") = RequestBuilder().url(url)
+
+    @JvmStatic
+    fun get(url: String) = RequestBuilder().url(url).method(RequestMethod.GET)
+
+    @JvmStatic
+    fun post(url: String) = RequestBuilder().url(url).method(RequestMethod.POST)
+
+    @JvmStatic
+    fun setClient(client: OkHttpClient): OkConnection {
+        mHttpClient = client
+        return this
+    }
+
+    @JvmStatic
+    fun Request.call() = RequestHolder(this)
+
+    @JvmStatic
+    fun Request.syncCall() = SyncRequestHolder(this)
 }
